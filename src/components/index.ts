a90d444a56d02dfc8de2b49d9c8d77505fa1177e
Brady Peterson <bpeterson1663@gmail.com>
2021-08-03 21:57:21 -0500

    feat(nav bar): create nav bar conponent

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import NavBar from './nav-bar/nav-bar.component'
+
+export {
+    NavBar
+}
---
+++ src/components/nav-bar/nav-bar.component.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function NavBar(): JSX.Element {
+    return (
+        <nav>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="list-group-item">
+                    <Link to="/movies">Movies</Link>
+                </li>
+                <li className="list-group-item">
+                    <Link to="/admin">Manage Catalogue</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
