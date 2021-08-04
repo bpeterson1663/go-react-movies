836dfd1d8f2a68e3ceb04aa9a7bd8e48d2899ca1
Brady Peterson <bpeterson1663@gmail.com>
2021-08-03 21:57:33 -0500

    feat(app): implement navbar and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { Admin, Movies, Home} from './pages' 
+import { NavBar }  from './components'
 function App() {
   return (
     <Router>
@@ -11,26 +13,13 @@ function App() {
 
       <div className="row">
         <div className="col-md-2">
-          <nav>
-            <ul className="list-group">
-              <li className="list-group-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/movies">Movies</Link>
-              </li>
-              <li className="list-group-item">
-                <Link to="/admin">Manage Catalogue</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavBar />
         </div>
         <div className="col-md-10">
           <Switch>
             <Route path="/movies" component={Movies} />
             <Route path="/admin" component={Admin} />
             <Route path="/" component={Home} />
-
           </Switch>
         </div>
       </div>
@@ -41,21 +30,3 @@ function App() {
 }
 
 export default App;
-
-function Home() {
-  return (
-    <h2>Home</h2>
-  )
-}
-
-function Admin() {
-  return (
-    <h2>Admin</h2>
-  )
-}
-
-function Movies() {
-  return (
-    <h2>Movies</h2>
-  )
-}
