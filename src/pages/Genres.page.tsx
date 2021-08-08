69a069ee746a2d2b93592494347ac3678392db11
Brady Peterson <bpeterson1663@gmail.com>
2021-08-08 14:50:00 -0500

    feat(genres): get and display all genres

--- src/pages/Genres.page.tsx
+++ src/pages/Genres.page.tsx
@@ -1,16 +1,48 @@
-import React from 'react'
-import {useRouteMatch, Link} from 'react-router-dom'
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { FetchStatusT } from '../constants'
+interface GenreT {
+  id: string
+  genreName: string
+}
 
 export default function Genres() {
-    let { path } = useRouteMatch()
+    const [genres, setGenres] = useState<GenreT[]>([])
+    const [fetchStatus, setFetchStatus] = useState<FetchStatusT>('idle')
+    const [error, setError] = useState('')
+    const getGenres = async () => {
+        setFetchStatus('pending')
+        try {
+            const response = await fetch(`http://localhost:4000/v1/genres`)
+            if(response.status !== 200) {
+                setFetchStatus('error')
+                setError(`An Error Occurred: ${response.statusText}`)
+            }
+            const { genres } = await response.json()
+            setGenres(genres)
+            setFetchStatus('success') 
+        } catch (err) {
+            setFetchStatus('error')
+        }
+    }
+
+    useEffect(() => {
+        getGenres()
+    }, [])
     return (
-      <div>
-      <h2>Genres</h2>
-      <ul>
-        <li><Link to={`${path}/comedy`}>Comedy</Link></li>
-  
-        <li><Link to={`${path}/drama`}>Drama</Link></li>
-      </ul>
-      </div>
+      <>
+          <h2>Genres</h2>
+          {fetchStatus === 'pending' && <h3>Loading...</h3>}
+          {fetchStatus === 'error' && <h3>{error}</h3>}
+          {fetchStatus === 'success' && 
+            <ul>
+                {genres.map( genre => (
+                    <li key={genre.id}>
+                        <Link to={`/genre/${genre.id}`}>{genre.genreName}</Link>
+                    </li>
+                ))}
+            </ul>
+          }
+        </>
     )
   }
