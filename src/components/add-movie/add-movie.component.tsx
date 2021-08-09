5b7e7431e8af459e7c4ea534721e288f225270fd
Brady Peterson <bpeterson1663@gmail.com>
2021-08-08 20:10:48 -0500

    feat(add movie): add controlled form elements

--- src/components/add-movie/add-movie.component.tsx
+++ src/components/add-movie/add-movie.component.tsx
@@ -1,30 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { MovieT } from '../../constants'
 import './add-movie.styles.css'
 
+const DEFAULT_MOVIE: MovieT = {
+    id: "0",
+    title: "",
+    releaseDate: "",
+    runtime: 0,
+    mpaaRating: "G",
+    rating: "",
+    description: "",
+    genres: []
+}
 export default function AddMovie () {
-    const [movie, setMovie] = useState<MovieT>({} as MovieT)
+    const [movie, setMovie] = useState<MovieT>(DEFAULT_MOVIE)
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let value = event.target.value
+        let name = event.target.name 
+        setMovie((values) => ({ ...values, [name]: value }))
+    }
 
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        let value = event.target.value
+        let name = event.target.name 
+        setMovie((values) => ({ ...values, [name]: value }))
+    }
+
+    const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        let value = event.target.value
+        let name = event.target.name 
+        setMovie((values) => ({ ...values, [name]: value }))
+    }
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault()
+    }
     return (
         <>
         <h2>Add/Edit Movie</h2>
         <hr />
-        <form method="post">
+        <form onSubmit={handleSubmit}>
+            <input type="hidden" name="id" id="id" value={movie.id} onChange={handleInputChange} />
             <div className="mb-3">
                 <label htmlFor="title" className="form-label">Title</label>
-                <input type="text" className="form-control" id="title" name="title" value={movie.title}/>
+                <input type="text" className="form-control" id="title" name="title" value={movie.title} onChange={handleInputChange}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="releaseDate" className="form-label">Release Date</label>
-                <input type="text" className="form-control" id="releaseDate" name="releaseDate" value={movie.releaseDate}/>
+                <input type="text" className="form-control" id="releaseDate" name="releaseDate" value={movie.releaseDate} onChange={handleInputChange}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="runtime" className="form-label">Runtime</label>
-                <input type="text" className="form-control" id="runtime" name="runtime" value={movie.runtime}/>
+                <input type="text" className="form-control" id="runtime" name="runtime" value={movie.runtime} onChange={handleInputChange}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="mpaaRating" className="form-label">MPAA Rating</label>
-                <select className="form-select" value={movie.mpaaRating}>
+                <select name="mpaaRating" className="form-select" value={movie.mpaaRating} onChange={handleSelectChange}>
                     <option className="form-select">Choose...</option>
                     <option className="form-select" value="G">G</option>
                     <option className="form-select" value="PG">PG</option>
@@ -35,13 +66,11 @@ export default function AddMovie () {
             </div>
             <div className="mb-3">
                 <label htmlFor="rating" className="form-label">Rating</label>
-                <input type="text" className="form-control" id="rating" name="rating" value={movie.rating}/>
+                <input type="text" className="form-control" id="rating" name="rating" value={movie.rating} onChange={handleInputChange}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="description" className="form-label">Description</label>
-                <textarea className="form-control" rows={3} id="description" name="description">
-                    {movie.description}
-                </textarea>
+                <textarea className="form-control" rows={3} id="description" name="description" onChange={handleTextareaChange} value={movie.description} />
             </div>
             <hr />
 
