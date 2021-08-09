3ca748ca48aec454fb817b4a158c60d2df837355
Brady Peterson <bpeterson1663@gmail.com>
2021-08-08 22:02:06 -0500

    style(prettier): ran prettier

--- src/components/form-fields/form-input.component.tsx
+++ src/components/form-fields/form-input.component.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
 
 interface FormInputT {
-    label: string,
-    name: string,
-    id: string,
-    value: string | number,
-    type: 'text' | 'number' | 'hidden'
-    handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  label: string
+  name: string
+  id: string
+  value: string | number
+  type: 'text' | 'number' | 'hidden'
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function FormInput({handleChange, value, id, name, label, type}: FormInputT): JSX.Element {
-    return (
-        <div className="mb-3">
-          <label htmlFor={label} className="form-label">
-            {label}
-          </label>
-          <input
-            type={type}
-            className="form-control"
-            id={id}
-            name={name}
-            value={value}
-            onChange={handleChange}
-          />
-        </div>
-    )
+export default function FormInput({ handleChange, value, id, name, label, type }: FormInputT): JSX.Element {
+  return (
+    <div className="mb-3">
+      <label htmlFor={label} className="form-label">
+        {label}
+      </label>
+      <input type={type} className="form-control" id={id} name={name} value={value} onChange={handleChange} />
+    </div>
+  )
 }
--- src/components/form-fields/form-textarea.component.tsx
+++ src/components/form-fields/form-textarea.component.tsx
@@ -1,28 +1,21 @@
 import React from 'react'
 
 interface FormTextareaT {
-    label: string,
-    name: string,
-    id: string,
-    value: string,
-    rows: number,
-    handleChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
+  label: string
+  name: string
+  id: string
+  value: string
+  rows: number
+  handleChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
-export default function FormTextarea({handleChange, value, rows, label, id, name}: FormTextareaT): JSX.Element {
-    return (
-        <div className="mb-3">
-          <label htmlFor={label} className="form-label">
-            {label}
-          </label>
-          <textarea
-            className="form-control"
-            rows={rows}
-            id={id}
-            name={name}
-            onChange={handleChange}
-            value={value}
-          />
-        </div>
-    )
+export default function FormTextarea({ handleChange, value, rows, label, id, name }: FormTextareaT): JSX.Element {
+  return (
+    <div className="mb-3">
+      <label htmlFor={label} className="form-label">
+        {label}
+      </label>
+      <textarea className="form-control" rows={rows} id={id} name={name} onChange={handleChange} value={value} />
+    </div>
+  )
 }
