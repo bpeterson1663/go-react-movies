2f03115907f6e1543d1cba5ae3c26c64ab8b877d
Brady Peterson <bpeterson1663@gmail.com>
2021-08-13 22:21:22 -0500

    feat(form input): add required attribute and default to false

--- src/components/form-fields/form-input.component.tsx
+++ src/components/form-fields/form-input.component.tsx
@@ -5,17 +5,18 @@ interface FormInputT {
   name: string
   id: string
   value?: string | number
+  required?: boolean
   type: 'text' | 'number' | 'hidden' | 'email' | 'password'
   handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export default function FormInput({ handleChange, value, id, name, label, type }: FormInputT): JSX.Element {
+export default function FormInput({ handleChange, value, id, name, label, type, required = false }: FormInputT): JSX.Element {
   return (
     <div className="mb-3">
       <label htmlFor={label} className="form-label">
         {label}
       </label>
-      <input type={type} className="form-control" id={id} name={name} value={value} onChange={handleChange} />
+      <input type={type} required={required} className="form-control" id={id} name={name} value={value} onChange={handleChange} />
     </div>
   )
 }
