74b4ecc9a5e3bad422c0593cd7645ee7fa164b46
Brady Peterson <bpeterson1663@gmail.com>
2021-08-13 22:37:22 -0500

    feat(auth): get and set token

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router'
 import { Alert, FormInput } from '../components'
 import { AlertT, UserAuthT } from '../constants'
 interface LoginT {
@@ -7,25 +8,47 @@ interface LoginT {
 export default function Login({ handleJWTChange }: LoginT) {
   const [userAuth, setUserAuth] = useState<UserAuthT>({} as UserAuthT)
   const [alert, setAlert] = useState<AlertT>({ alertType: 'd-none', message: '' })
+  const history = useHistory()
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let value = event.target.value
     let name = event.target.name
     setUserAuth((values) => ({ ...values, [name]: value }))
   }
 
-  const handleSubmit = (event: React.FormEvent) => {
+  const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault()
-    console.log('userAuth: ', userAuth)
-    setAlert({ alertType: 'alert-success', message: 'Login Success!' })
+    try {
+      const response = await fetch('http://localhost:4000/v1/signin', {
+        method: 'POST',
+        body: JSON.stringify(userAuth),
+      })
+      if (response.status !== 200) {
+        setAlert({ alertType: 'alert-danger', message: response.statusText })
+      } else {
+        const { token } = await response.json()
+        setAlert({ alertType: 'alert-success', message: 'Login Success!' })
+        handleJWTChange(token)
+        history.push('/')
+      }
+    } catch (err) {
+      setAlert({ alertType: 'alert-danger', message: err.message })
+    }
   }
 
   return (
     <>
       <h2>Login</h2>
       <Alert alertType={alert.alertType} message={alert.message} />
-    <form onSubmit={handleSubmit} className="pt-3">
+      <form onSubmit={handleSubmit} className="pt-3">
         <FormInput required id="email" label="Email" type="email" name="email" handleChange={handleInputChange} />
-        <FormInput required id="password" label="Password" type="password" name="password" handleChange={handleInputChange} />
+        <FormInput
+          required
+          id="password"
+          label="Password"
+          type="password"
+          name="password"
+          handleChange={handleInputChange}
+        />
         <hr />
         <button type="submit" className="btn btn-primary">
           Login
