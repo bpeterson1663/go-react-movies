df38e29f7c25183ef36f4ab6f43ddc93cb6f4b9d
Brady Peterson <bpeterson1663@gmail.com>
2021-08-15 06:01:24 -0500

    feat(admin): create admin page

--- src/pages/Admin.page.tsx
+++ src/pages/Admin.page.tsx
@@ -1,5 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FetchStatusT } from '../constants'
+
+interface MovieT {
+  id: number
+  title: string
+  runtime: number
+}
 
 export default function Admin(): JSX.Element {
-  return <h2>Manage Catalogue</h2>
+  const [movies, setMovies] = useState<MovieT[]>([])
+  const [fetchStatus, setFetchStatus] = useState<FetchStatusT>('idle')
+  const [error, setError] = useState('')
+  const getMovies = async () => {
+    setFetchStatus('pending')
+    try {
+      const response = await fetch('http://localhost:4000/v1/movies')
+      if (response.status !== 200) {
+        setFetchStatus('error')
+        setError(`An Error Occurred: ${response.statusText}`)
+      }
+      const { movies } = await response.json()
+      setFetchStatus('success')
+      setMovies(movies)
+    } catch (err) {
+      setFetchStatus('error')
+    }
+  }
+  useEffect(() => {
+    getMovies()
+  }, [])
+  return (
+    <>
+      <h2>Movies</h2>
+      {fetchStatus === 'pending' && <h3>Loading...</h3>}
+      {fetchStatus === 'error' && <h3>{error}</h3>}
+      {fetchStatus === 'success' && (
+        <div className="list-group">
+          {movies.map((m) => (
+            <Link key={m.id} to={`/admin/movie/${m.id}`} className="list-group-item list-group-item-action">
+              {m.title}
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
+  )
 }
